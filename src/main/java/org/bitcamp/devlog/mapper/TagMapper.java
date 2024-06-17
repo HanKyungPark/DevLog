d8612b180193595272dce7c6d8d532b873d80023
noah <jeonghogim318@gmail.com>
2024-06-17 20:03:31 +0900

    mapper 검수

--- src/main/java/org/bitcamp/devlog/mapper/TagMapper.java
+++ src/main/java/org/bitcamp/devlog/mapper/TagMapper.java
@@ -6,6 +6,11 @@ import org.bitcamp.devlog.dto.Tag;
 
 @Mapper
 public interface TagMapper {
+
+    //태그검색, 태그추가
     Long findTagIdByTagName(String tagName);
+
+    //태그추가
     void save(Tag tag);
 }
+
