eb05d2f7d2553b629f0d140a19baa92eac199a48
songawon <ok466sgw@naver.com>
2024-06-17 14:56:53 +0900

    레이아웃 + visit mapper 작성

--- src/main/java/org/bitcamp/devlog/controller/HomeServCotroller.java
+++ src/main/java/org/bitcamp/devlog/controller/HomeServCotroller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeServCotroller {
     @GetMapping("/")
     public String home() {
-        return "layout/layout_ele/header";
+        return "contents/mainPage";
     }
 }
