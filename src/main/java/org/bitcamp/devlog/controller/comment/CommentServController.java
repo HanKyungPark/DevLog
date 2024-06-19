b96194c99b4709151669b8d8266bf997461dace8
noah <jeonghogim318@gmail.com>
2024-06-19 10:51:30 +0900

    account mapper, xml작성

--- src/main/java/org/bitcamp/devlog/controller/comment/CommentServController.java
+++ src/main/java/org/bitcamp/devlog/controller/comment/CommentServController.java
@@ -1,7 +1,10 @@
 package org.bitcamp.devlog.controller.comment;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class CommentServController {
+
 }
