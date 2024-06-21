f9361914b74e75b876bcc40c89a8670d11961e31
wancheol <tlsdhks543@naver.com>
2024-06-21 14:54:03 +0900

    홈페이지 수정

--- src/main/java/org/bitcamp/devlog/mapper/TagMapper.java
+++ src/main/java/org/bitcamp/devlog/mapper/TagMapper.java
@@ -4,8 +4,6 @@ package org.bitcamp.devlog.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.bitcamp.devlog.dto.Tag;
 
-import java.util.List;
-
 @Mapper
 public interface TagMapper {
 
@@ -14,7 +12,5 @@ public interface TagMapper {
 
     //태그추가
     void save(Tag tag);
-
-    String findTagNameByTag_id(Long tagId);
 }
 
--- src/main/java/org/bitcamp/devlog/service/AccountService.java
+++ src/main/java/org/bitcamp/devlog/service/AccountService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -52,6 +53,11 @@ public class AccountService {
         }
 
         return accountMapper.findByEmail(sessionEmail).getHomepage();
+    }
+
+    public List<Account> findAll()
+    {
+        return accountMapper.findAll();
     }
 
 }
