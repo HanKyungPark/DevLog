1ff4da1c62e5d506a869aafc532bea1fdeecc599
hankyung <gksrud246@naver.com>
2024-06-24 16:35:49 +0900

    MyBlog css변경

--- src/main/resources/static/js/myBlogPage.js
+++ src/main/resources/static/js/myBlogPage.js
@@ -24,29 +24,29 @@ $(function () {
                 // 각 포스트 데이터를 HTML로 변환하여 추가
                 data.forEach(function(post, idx) {
                     postElement += `
-                    <div class="blogpost_box" data-v0-t="card" id="blogpost_box${idx+1}">
-                        <a href="#">
-                            <img
-                                src="https://minio.bmops.kro.kr/devlog/${post.file}"
-                                alt="Featured Post"
-                                class="blog_photo"
-                            />
-                        </a>
-                        <div class="post_content">
-                            <div class="post_header">
-                                <div class="category_inpost">Design</div>
-                                <div class="post_date">${post.pcreatedAt}</div>
-                                <div class="icon-container">
-                                    &nbsp;&nbsp;<i class="bi bi-chat-dots">2</i>
-                                    &nbsp;<i class="bi bi-heart"></i>
-                                </div>
-                            </div>
-                            <a class="post_title_container" href="#">
-                                <h2 class="post_title">${post.title}</h2>
-                            </a>
-                            <p class="text-gray-600">${post.pcontent}</p>
+                  <div class="blogpost_box" data-v0-t="card" id="blogpost_box${idx+1}">
+                    <a href="#">
+                      <img
+                        src="https://minio.bmops.kro.kr/devlog/${post.file}"
+                        alt="Featured Post"
+                        class="blog_photo"
+                      />
+                    </a>
+                    <div class="post_content">
+                      <div class="post_header">
+                        <div class="category_inpost">${post.category}</div>
+                        <div class="icon-container">
+                          &nbsp;&nbsp;<i class="bi bi-chat-dots">2</i>
+                          &nbsp;<i class="bi bi-heart"></i>
                         </div>
-                    </div>`;
+                      </div>
+                      <a class="post_title_container" href="#">
+                        <h2 class="post_title">${post.title}</h2>
+                      </a>
+                      <!--<p class="text-gray-600">${post.pcontent}</p> -->
+                      <div class="post_date">${post.pcreatedAt}</div> <!-- pcreatedAt 요소를 하단으로 이동 -->
+                    </div>
+                  </div>`;
                 });
 
                 postElement += '</div>'; // 포스트 컨테이너 닫기
