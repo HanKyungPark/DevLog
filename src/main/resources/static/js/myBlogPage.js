53675e7e0a1d1ae2eb6c09dfc24b1b20ed97933f
hankyung <gksrud246@naver.com>
2024-06-25 14:54:04 +0900

    myblogPage detail 페이지 이동

--- src/main/resources/static/js/myBlogPage.js
+++ src/main/resources/static/js/myBlogPage.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    let postUrl = "";
     let pathname = window.location.pathname.split("/")[1];
     $.ajax({
         url: "api/blog/check",
@@ -13,55 +15,61 @@ $(function () {
         }
     });
 
-    $(document).ready(function() {
-        let pathname = window.location.pathname.split("/")[1];
 
-        // AJAX 요청
-        $.ajax({
-            url: "/api/post/myblog/list",
-            type: "POST",
-            data: {"homepage": pathname},
-            dataType: "json",
-            success: function(data) {
-                let postElement = '<div class="post_container">'; // 포스트 컨테이너 시작
 
-                // 각 포스트 데이터를 HTML로 변환하여 추가
-                data.forEach(function(post, idx) {
-                    postElement += `
-                  <div class="blogpost_box" data-v0-t="card" id="blogpost_box${idx+1}">
+    // AJAX 요청
+    $.ajax({
+        url: "/api/post/myblog/list",
+        type: "POST",
+        data: {"homepage": pathname},
+        dataType: "json",
+        success: function(data) {
+
+            let postElement = '<div class="post_container">'; // 포스트 컨테이너 시작
+
+            // 각 포스트 데이터를 HTML로 변환하여 추가
+            data.forEach(function(post, idx) {
+                postUrl = post.postUrl;
+                postElement += `
+                <div class="blogpost_box" data-v0-t="card" id="blogpost_box${idx+1}">
                     <a href="#">
-                      <img
-                        src="https://minio.bmops.kro.kr/devlog/${post.file}"
-                        alt="Featured Post"
-                        class="blog_photo"
-                      />
+                        <img
+                            src="https://minio.bmops.kro.kr/devlog/${post.file}"
+                            alt="Featured Post"
+                            class="blog_photo"
+                        />
                     </a>
                     <div class="post_content">
-                      <div class="post_header">
-                        <div class="category_inpost">${post.category}</div>
-                        <div class="icon-container">
-                          &nbsp;&nbsp;<i class="bi bi-chat-dots">2</i>
-                          &nbsp;<i class="bi bi-heart"></i>
+                        <div class="post_header">
+                            <div class="category_inpost">${post.category}</div>
+                            <div class="icon-container">
+                                &nbsp;&nbsp;<i class="bi bi-chat-dots">2</i>
+                                &nbsp;<i class="bi bi-heart"></i>
+                            </div>
                         </div>
-                      </div>
-                      <a class="post_title_container" href="#">
-                        <h2 class="post_title">${post.title}</h2>
-                      </a>
-                      <!--<p class="text-gray-600">${post.pcontent}</p> -->
-                      <div class="post_date">${formatDate(post.pcreatedAt)}</div> <!-- formatDate 함수를 적용 -->
+                        <a class="post_title_container" href="#">
+                            <h2 class="post_title">${post.title}</h2>
+                        </a>
+                        <!--<p class="text-gray-600">${post.pcontent}</p> -->
+                        <div class="post_date">${formatDate(post.pcreatedAt)}</div> <!-- formatDate 함수를 적용 -->
                     </div>
-                  </div>`;
-                });
+                </div>`;
+            });
 
-                postElement += '</div>'; // 포스트 컨테이너 닫기
-                $(".post_container").append(postElement); // HTML 추가
+            postElement += '</div>'; // 포스트 컨테이너 닫기
+            $(".post_container").append(postElement); // HTML 추가
 
-                // 추가한 포스트에 CSS 적용
-                $(".blogpost_box").each(function() {
-                    $(this).addClass("blogpost_box"); // 클래스 추가
-                });
-            }
-        });
+            // 추가한 포스트에 CSS 적용
+            $(".blogpost_box").each(function() {
+                $(this).addClass("blogpost_box"); // 클래스 추가
+            });
+        }
+    });
+    $(document).on("click", ".blogpost_box", function() {
+
+        let homepage = pathname;
+
+        location.href = '/' + homepage + "/" + postUrl + "/detail";
     });
 });
 
