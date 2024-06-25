98039dc531d31fd0b83e94775c87081f1379c0f8
noah <jeonghogim318@gmail.com>
2024-06-25 09:58:13 +0900

    mypage pagnation

--- src/main/resources/static/js/myPage.js
+++ src/main/resources/static/js/myPage.js
@@ -1,3 +1,10 @@
+let postCurrentPage = 0;
+const postPageSize = 5;
+let posts = [];
+let commentCurrentPage = 0;
+const commentPageSize = 8;
+let comments = [];
+let file;
 
 //포스트가져와
 function loadPosts() {
@@ -5,65 +12,10 @@ function loadPosts() {
     url: '/api/mypage/posts',
     method: 'GET',
     success: function(data) {
-      let postsHtml = '';
-
-      if(data.length === 0) {
-
-        postsHtml = `<h2>게시글이 없습니다</h2>`
-
-      } else {
-        data.forEach(function(post) {
-
-          // Date 객체 생성
-          const date = new Date(post.pcreatedAt);
-
-          // 날짜 관련 정보 추출
-          const year = date.getFullYear(); // 연도
-          const month = date.getMonth() + 1; // 월 (0부터 시작하므로 +1 필요)
-          const day = date.getDate(); // 일
-
-          // 원하는 형식으로 날짜 문자열 생성
-          const formattedDate = `${year}년 ${month < 10 ? '0' + month : month}월 ${day < 10 ? '0' + day : day}일`;
-
-          postsHtml += `
-                    <div class="blog_post_box" style="display:flex; align-items: center; justify-content: space-between">
-                    <a class="blogpost_first" id="blogpost_third" href="${post.postUrl}">
-                        <img
-                            src="${post.file}"
-                            width="80"
-                            height="80"
-                            alt="Blog post thumbnail"
-                            class="post_img"
-                            id="post_img_third"
-                            style="aspect-ratio: 80 / 80; object-fit: cover;"
-                        />
-                        <div class="firstpost_container" id="third_container">
-                            <h3 class="firstpost_title" id="thirdpost_title">
-                              ${formattedDate}
-                            </h3>
-                            <p class="firstpost_content" id="third_content">
-                                ${post.title}
-                            </p>
-                        </div>
-                    </a>
-                    <div >
-                      <div>
-                        <a  href="/mypage/post/update/${post.postUrl}" class="btn btn-primary">수정</a>
-                      </div>
-                      <div>
-                        <button onclick="postDelete(this)" class="btn btn-danger">삭제</button>
-                      </div>
-                    </div>    
-                   </div>        
-              `;
-        });
-      }
-
-      $('#blogpost_inside').html(postsHtml);
-
-    },
-
-    error: function(error) {
+      console.log(data);
+      posts = data;
+      console.log(posts);
+    }, error: function(error) {
       console.error('Error fetching posts', error);
     }
   });
@@ -74,65 +26,16 @@ function loadComments() {
   $.ajax({
     url: '/api/mypage/comment',
     method: 'GET',
-    //댓글 릴스트 요청
+    //댓글 리스트 요청
     success: function(data) {
       let commentsHtml = '';
+      file = data.file;
+      comments = data.comments;
 
-      if(data.comments.length == 0){
+      console.log(file)
+      console.log(comments);
 
-        commentsHtml = `<h1>댓글이 없습니다.</h1>`
 
-      } else {
-
-          data.comments.forEach(function(comment) {
-
-          const date = new Date(comment.ccreatedAt);
-
-          // 날짜 관련 정보 추출
-          const year = date.getFullYear(); // 연도
-          const month = date.getMonth() + 1; // 월 (0부터 시작하므로 +1 필요)
-          const day = date.getDate(); // 일
-
-          // 원하는 형식으로 날짜 문자열 생성
-          const formattedDate = `${year}년 ${month < 10 ? '0' + month : month}월 ${day < 10 ? '0' + day : day}일`;
-
-          commentsHtml +=
-              `<div className="comments_frame" id="comments_frame3" style="display: flex; align-items: center; justify-content: space-between">
-                    
-                  <span className="user_img_frame" id="user_img_frame3">
-                    <img className="user_img" 
-                          id="user_img3"
-                          src="${data.file}"
-                          alt="user_image"
-                          width="50" height="50"
-                          style="border-radius: 50%;"
-                          />
-                  </span>
-            <div className="comments_allcontents" id="comments_allcontents3">
-
-              <div className="name_time" id="name_time3">
-                <div className="comments_name" id="comments_name3"></div>
-                <div className="comments_time" id="comments_time3">${formattedDate}</div>
-              </div>
-              <p className="comments_content" id="comments_content3">
-                ${comment.ccontent}
-              </p>
-            </div>
-            <div>
-                <div>
-                  <a  href="해당글의 상세페이지로 이동" class="btn btn-primary">수정</a>
-                </div>
-                <div>
-                  <button onclick="commentDelete(this)" class="btn btn-danger">
-                  삭제
-                  <p id="comment_id" style="display: none">${comment.commentId}</p>
-                  </button>
-                </div>
-              </div>
-          </div>
-            `
-        })
-      };
 
       $('#comments_box').html(commentsHtml);
     },
@@ -168,16 +71,140 @@ function loadVisitCount(){
   });
 };
 
+function displayPosts() {
+  const start = postCurrentPage * postPageSize;
+  const end = start + postPageSize;
+  const pagePosts = posts.slice(start, end);
+
+  $('#blogpost_inside').empty();
+
+  let postsHtml = '';
+
+  console.log(pagePosts);
+
+
+  pagePosts.forEach(function(post) {
+    // Date 객체 생성
+    const date = new Date(post.pcreatedAt);
+
+    // 날짜 관련 정보 추출
+    const year = date.getFullYear(); // 연도
+    const month = date.getMonth() + 1; // 월 (0부터 시작하므로 +1 필요)
+    const day = date.getDate(); // 일
+
+    // 원하는 형식으로 날짜 문자열 생성
+    const formattedDate = `${year}년 ${month < 10 ? '0' + month : month}월 ${day < 10 ? '0' + day : day}일`;
+
+    postsHtml += `
+                    <div class="blog_post_box" style="display:flex; align-items: center; justify-content: space-between">
+                    <a class="blogpost_first" id="blogpost_third" href="${post.postUrl}">
+                        <img
+                            src="${post.file}"
+                            width="80"
+                            height="80"
+                            alt="Blog post thumbnail"
+                            class="post_img"
+                            id="post_img_third"
+                            style="aspect-ratio: 80 / 80; object-fit: cover;"
+                        />
+                        <div class="firstpost_container" id="third_container">
+                            <h3 class="firstpost_title" id="thirdpost_title">
+                              ${formattedDate}
+                            </h3>
+                            <p class="firstpost_content" id="third_content">
+                                ${post.title}
+                            </p>
+                        </div>
+                    </a>
+                    <div >
+                      <div>
+                        <a  href="/mypage/post/update/${post.postUrl}" class="btn btn-primary">수정</a>
+                      </div>
+                      <div>
+                        <button onclick="postDelete(this)" class="btn btn-danger">삭제</button>
+                      </div>
+                    </div>    
+                   </div>        
+              `;
+  });
+
+  $('#blogpost_inside').html(postsHtml);
+}
+
 $(document).ready(function() {
 
   // 포스트 목록 로드
   loadPosts()
+  displayPosts()
+  postToggleButtons()
 
   // 댓글 목록 로드
   loadComments();
 
+  function displayComments() {
+    
+    if(data.comments.length == 0){
+
+      commentsHtml = `<h1>댓글이 없습니다.</h1>`
+
+    } else {
+
+      data.comments.forEach(function(comment) {
+
+        const date = new Date(comment.ccreatedAt);
+
+        // 날짜 관련 정보 추출
+        const year = date.getFullYear(); // 연도
+        const month = date.getMonth() + 1; // 월 (0부터 시작하므로 +1 필요)
+        const day = date.getDate(); // 일
+
+        // 원하는 형식으로 날짜 문자열 생성
+        const formattedDate = `${year}년 ${month < 10 ? '0' + month : month}월 ${day < 10 ? '0' + day : day}일`;
+
+        commentsHtml +=
+            `<div className="comments_frame" id="comments_frame3" style="display: flex; align-items: center; justify-content: space-between">
+                    
+                  <span className="user_img_frame" id="user_img_frame3">
+                    <img className="user_img" 
+                          id="user_img3"
+                          src="${data.file}"
+                          alt="user_image"
+                          width="50" height="50"
+                          style="border-radius: 50%;"
+                          />
+                  </span>
+            <div className="comments_allcontents" id="comments_allcontents3">
+
+              <div className="name_time" id="name_time3">
+                <div className="comments_name" id="comments_name3"></div>
+                <div className="comments_time" id="comments_time3">${formattedDate}</div>
+              </div>
+              <p className="comments_content" id="comments_content3">
+                ${comment.ccontent}
+              </p>
+            </div>
+            <div>
+                <div>
+                  <a  href="해당글의 상세페이지로 이동" class="btn btn-primary">수정</a>
+                </div>
+                <div>
+                  <button onclick="commentDelete(this)" class="btn btn-danger">
+                  삭제
+                  <p id="comment_id" style="display: none">${comment.commentId}</p>
+                  </button>
+                </div>
+              </div>
+          </div>
+            `
+      })
+    };
+  }
+
+  displayComments()
+  commentToggleButtons()
   //방문자수 로드
   loadVisitCount();
+
 });
 
 
@@ -191,11 +218,10 @@ function postDelete(button){
         contentType: 'application/json',
         data: JSON.stringify({ postUrl: postUrl }),
         success: function(response) {
-          alert('Post updated successfully');
-          loadPosts()
+          displayPosts()
         },
         error: function(xhr, status, error) {
-          alert('Failed to update post');
+          alert('Failed to delete post');
         }
   })
 }
@@ -212,14 +238,37 @@ function commentDelete(button){
     contentType: 'application/json',
     data: JSON.stringify({ commentId: commentId }),
     success: function(response) {
-      alert('Post updated successfully');
       loadComments();
     },
     error: function(xhr, status, error) {
-      alert('Failed to update post');
+      alert('Failed to delete comment');
     }
   })
 }
+
+//이전페이지 이동
+function postPrevPage() {
+  if (postCurrentPage > 0) {
+    postCurrentPage--;
+    displayPosts();
+    postToggleButtons();
+  }
+}
+
+//다음 페이지 이동
+function postNextPage() {
+  if ((postCurrentPage + 1) * postPageSize < posts.length) {
+    postCurrentPage++;
+    displayPosts();
+    postToggleButtons();
+  }
+}
+
+function postToggleButtons() {
+  $('#prev-btn').prop('disabled', postCurrentPage <= 0);
+  $('#next-btn').prop('disabled', (postCurrentPage + 1) * postPageSize >= posts.length);
+}
+
 
 
 
