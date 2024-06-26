7ae5f956b18d6a11806f30a07e72e3a40d70e655
songawon <ok466sgw@naver.com>
2024-06-26 17:11:45 +0900

    postingForm 제이쿼리

--- src/main/resources/static/js/postingForm.js
+++ src/main/resources/static/js/postingForm.js
@@ -1,6 +1,6 @@
 $(function () {
     // file click event를 div에 넓게 줌
-    $('.thumbnail_container').click(function () {
+    $(".thumbnail_container").click(function (){
         $('#file').click(); // #file input 요소를 클릭
     });
 
