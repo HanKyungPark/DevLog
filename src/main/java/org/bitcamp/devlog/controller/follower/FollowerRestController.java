d8ae7950c64578b62ff53fc1868cc45faaef32b6
BMDevOps <kumupaf6998@naver.com>
2024-06-30 20:23:22 +0900

    계정, 카테고리, 팔로우 Swagger 어노테이션 작성

--- src/main/java/org/bitcamp/devlog/controller/follower/FollowerRestController.java
+++ src/main/java/org/bitcamp/devlog/controller/follower/FollowerRestController.java
@@ -1,7 +1,9 @@
 package org.bitcamp.devlog.controller.follower;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "팔로우 API" , description = "Follower 테이블에 해당하는 데이터 조회, 추가, 수정, 삭제 가능한 API Controller")
 public class FollowerRestController {
 }
--- src/main/java/org/bitcamp/devlog/controller/heart/HeartRestController.java
+++ src/main/java/org/bitcamp/devlog/controller/heart/HeartRestController.java
@@ -1,5 +1,10 @@
 package org.bitcamp.devlog.controller.heart;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.bitcamp.devlog.dto.Heart;
 import org.bitcamp.devlog.service.HeartService;
@@ -11,10 +16,16 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "좋아요 API", description = "Heart 테이블에 해당하는 데이터 조회, 수정, 추가 ,삭제 API Controller")
 public class HeartRestController {
     private final HeartService heartService;
 
     @GetMapping("/heart/count")
+    @Operation(summary = "", description = "",
+            parameters = {
+                    @Parameter(in = ParameterIn.QUERY, name = "", description = "", required = true, example = "devlog")
+            })
+    @ApiResponse(responseCode = "200", description = "success")
     public Long countByPostId(@RequestParam("postId") Long postId) {
         return heartService.countHeartByPostId(postId);
     }
